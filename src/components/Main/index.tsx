c8c4ff0bbc8f83a5b52052b789e6bdf4c7195311
Kevin Cruz <kevin_mcp@outlook.com>
2021-07-08 23:57:48 -0400

    estilos y textos complementares

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,7 +3,11 @@ import styles from './styles.module.scss'
 export function Main() {
   return (
     <div className={styles.container}>
-      <h1>Home</h1>
+      <h3>Home do painel de administrador</h3>
+      <span>Aqui teremos informação sobre os principais KPI que tem a empresa
+        onde se priorizará aquelas que estão com perigo de falhar por recursos
+        ou outros motivos.
+      </span>
     </div>
   )
 }
