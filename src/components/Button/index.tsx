e53670c4362a69985992e056820127230a3cbbe8
Leandro Cruz <leandro.programmer@hotmail.com>
2021-07-19 18:25:20 -0300

    add Button y inicializando el WidgetLarge

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.scss'
+
+export function Button() {
+  return <button className={styles.button}>Aprovar</button>
+}
--- src/components/Home/WidgetLarge/index.tsx
+++ src/components/Home/WidgetLarge/index.tsx
@@ -1,9 +1,63 @@
+import Image from 'next/image'
+
+import { Button } from '../../Button'
+
+import AvatarImg from '../../../../public/avatar.jpg'
+
 import styles from './styles.module.scss'
 
 export function WidgetLarge() {
   return (
     <div className={styles.container}>
-      <h1>Small</h1>
+      <h3>Novos Clientes Registrados</h3>
+
+      <table className="table">
+        <tr>
+          <th>Custumer</th>
+          <th>Date</th>
+          <th>Amount</th>
+          <th>Status</th>
+        </tr>
+
+        <tr>
+          <td>
+            <Image src={AvatarImg} alt="Avatar" />
+
+            <span>Leandro Cruz</span>
+          </td>
+          <td>2 Jun 2021</td>
+          <td>$122.00</td>
+          <td>
+            <Button />
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <Image src={AvatarImg} alt="Avatar" />
+
+            <span>Leandro Cruz</span>
+          </td>
+          <td>2 Jun 2021</td>
+          <td>$122.00</td>
+          <td>
+            <Button />
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <Image src={AvatarImg} alt="Avatar" />
+
+            <span>Leandro Cruz</span>
+          </td>
+          <td>2 Jun 2021</td>
+          <td>$122.00</td>
+          <td>
+            <Button />
+          </td>
+        </tr>
+      </table>
     </div>
   )
 }
