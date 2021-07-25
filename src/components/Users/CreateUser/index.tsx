71b52b8071147503f1d63fd12dbd55c6055fa4d0
Leandro Cruz <leandro.programmer@hotmail.com>
2021-07-24 21:06:21 -0300

    add Header en User

--- src/components/Users/CreateUser/index.tsx
+++ src/components/Users/CreateUser/index.tsx
@@ -1,10 +1,13 @@
 import { Button } from '../../Button'
+import { Header } from '../../Header'
 
 import styles from './styles.module.scss'
 
 export function CreateUser() {
   return (
     <div className={styles.container}>
+      <Header />
+
       <div className={styles.header}>
         <h1>Usuario</h1>
         <Button>Criar</Button>
