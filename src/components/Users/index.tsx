d437acec0f08cf9f4b1d87294c90c11a3bcba7c2
leh <leandro.programmer@hotmail.com>
2021-09-26 02:54:56 -0300

    changes on Modal

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -4,7 +4,18 @@ import Image from 'next/image'
 import Modal from 'react-modal'
 
 import { DataGrid } from '@material-ui/data-grid'
-import { Edit2, X, User, Mail, Users as Gen, Watch, ToggleLeft, UserCheck } from '@styled-icons/feather'
+import {
+  Edit2,
+  X,
+  User,
+  Mail,
+  Users as Gen,
+  Watch,
+  ToggleLeft,
+  UserCheck,
+  Upload,
+  UserPlus
+} from '@styled-icons/feather'
 
 import { Button } from '../Button'
 import { Header } from '../Header'
@@ -64,7 +75,7 @@ export function Users() {
           <>
             {/* <Link passHref href={'/user/' + params.row.id}> */}
             <Link passHref href="#">
-              <Button>
+              <Button onClick={handleOpenModal} >
                 <Edit2 className={styles.columnUserButtonEdit} />
                 Editar
               </Button>
@@ -200,57 +211,123 @@ export function Users() {
           disableSelectionOnClick
         />
 
-        <Button onClick={handleOpenModal}>Crear</Button>
+        <Button onClick={handleOpenModal}>
+          <UserPlus />
+          Criar
+        </Button>
 
-        <Modal isOpen={isOpenModalUser} onRequestClose={handleCloseModal}>
-          <div className={styles.user}>
-            <header>
-              <h1>Edit User</h1>
-              <X className={styles.buttonClose} onClick={handleCloseModal} />
-            </header>
-          </div>
-
-          <div className={styles.userContainer}>
-            <div className={styles.userShow}>
-              <header>
-                <Image src={AvatarImg} alt="Avatar" />
-                <div className={styles.userShowHeaderInfo}>
-                  <span className={styles.name}>Leandro Cruz</span>
-                  <span className={styles.moreInfo}>Software Engineer</span>
-                </div>
-              </header>
-
-              <main>
-                <span className={styles.titleMain}>Account Details</span>
-                <div className={styles.userInfo}>
-                  <User />
-                  <span>Leandro Cruz</span>
-                </div>
-                <div className={styles.userInfo}>
-                  <Gen />
-                  <span>masculino</span>
-                </div>
-                <div className={styles.userInfo}>
-                  <Watch />
-                  <span>24 anos</span>
-                </div>
-                <span className={styles.titleMain}>Contact Details</span>
-                <div className={styles.userInfo}>
-                  <Mail />
-                  <span>leandro.cruz@hotmail.com</span>
-                </div>
-                <div className={styles.userInfo}>
-                  <UserCheck />
-                  <span>admin: sim</span>
-                </div>
-                <div className={styles.userInfo}>
-                  <ToggleLeft />
-                  <span>estado: activo</span>
-                </div>
-              </main>
+        <Modal
+          isOpen={isOpenModalUser}
+          onRequestClose={handleCloseModal}
+          className={styles.Modal}
+          overlayClassName={styles.Overlay}
+        >
+          <div className={styles.wrapper}>
+            <div className={styles.userHeader}>
+                <h2>Editar Usuario</h2>
+                <X className={styles.buttonClose} onClick={handleCloseModal} />
             </div>
 
-            <div className={styles.userUpdate}></div>
+            <div className={styles.userContainer}>
+              <div className={styles.userShow}>
+                <header>
+                  <Image src={AvatarImg} alt="Avatar" />
+                  <div className={styles.userShowHeaderInfo}>
+                    <span className={styles.name}>Leandro Cruz</span>
+                    <span className={styles.moreInfo}>Software Engineer</span>
+                  </div>
+                </header>
+
+                <main>
+                  <span className={styles.titleMain}>Detalhes do Usuario</span>
+                  <div className={styles.userInfo}>
+                    <User />
+                    <span>Leandro Cruz</span>
+                  </div>
+                  <div className={styles.userInfo}>
+                    <Gen />
+                    <span>masculino</span>
+                  </div>
+                  <div className={styles.userInfo}>
+                    <Watch />
+                    <span>24 anos</span>
+                  </div>
+                  <span className={styles.titleMain}>Contato</span>
+                  <div className={styles.userInfo}>
+                    <Mail />
+                    <span>leandro.cruz@hotmail.com</span>
+                  </div>
+                  <span className={styles.titleMain}>Detalhes da Conta</span>
+                  <div className={styles.userInfo}>
+                    <UserCheck />
+                    <span>admin: sim</span>
+                  </div>
+                  <div className={styles.userInfo}>
+                    <ToggleLeft />
+                    <span>estado: ativo</span>
+                  </div>
+                </main>
+              </div>
+
+              <div className={styles.userUpdate}>
+                <h3>Atualizar Dados</h3>
+
+                <form>
+                  <div className={styles.formLeft}>
+                    <div className={styles.updateItem}>
+                      <label>Nome</label>
+                      <input type="text" placeholder="Maria Silveira" />
+                    </div>
+                    <div className={styles.updateItem}>
+                      <label>Gênero</label>
+                      <input type="text" placeholder="Masculino / Femenino" />
+                    </div>
+                    <div className={styles.updateItem}>
+                      <label>Edade</label>
+                      <input type="number" placeholder="12" />
+                    </div>
+                    <div className={styles.updateItem}>
+                      <label>Email</label>
+                      <input
+                        type="text"
+                        placeholder="maria.silveira@hotmai.com"
+                      />
+                    </div>
+                    <div className={styles.updateItem}>
+                      <label>Contrasenha</label>
+                      <input type="password" placeholder="********" />
+                    </div>
+                    <div className={styles.updateItem}>
+                      <label>Admin</label>
+                      <input type="text" placeholder="Sim / Não" />
+                    </div>
+                    <div className={styles.updateItem}>
+                      <label>Estado</label>
+                      <input type="text" placeholder="Ativo / Inativo" />
+                    </div>
+                  </div>
+
+                  <div className={styles.formRight}>
+                    <div className={styles.formImgContainer}>
+                      <Image src={AvatarImg} alt="Avatar" />
+                      <label htmlFor="file">
+                        <Upload />
+                      </label>
+                      <input
+                        type="file"
+                        id="file"
+                        style={{ display: 'none' }}
+                      />
+                    </div>
+
+                    <Button type="submit">
+                      <Edit2 />
+                      Atualizar
+                    </Button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </Modal>
       </div>
