38c00da90ebbcda4052c561dd90feeed5cc7b776
leh <leandro.programmer@hotmail.com>
2021-09-30 13:08:59 -0300

    responsividad en home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -9,15 +9,26 @@ export function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <FeaturedInfo title="Receita" amount="1022,42" rate="-1,2" />
-        <FeaturedInfo title="Ventas" amount="790,20" rate="-0,3" />
-        <FeaturedInfo title="Compras" amount="450,00" rate="-0,1" />
+        <div className={styles.featuredInfo}>
+          <FeaturedInfo title="Receita" amount="1022,42" rate="-1,2" />
+          <FeaturedInfo title="Ventas" amount="790,20" rate="-0,3" />
+          <FeaturedInfo title="Compras" amount="450,00" rate="-0,1" />
+        </div>
+
+        <div className={styles.repositionWidgetSmall}>
+          <WidgetSmall />
+        </div>
+
       </div>
 
+    <div className={styles.chart}>
       <Chart />
+    </div>
 
       <div className={styles.widget}>
-        <WidgetSmall />
+        <div className={styles.temporalWidgetSmall}>
+          <WidgetSmall />
+        </div>
         <WidgetLarge />
       </div>
     </div>
