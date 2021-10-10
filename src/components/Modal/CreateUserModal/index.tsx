8ce64cd44ad53239a741612124f2053df8d99c5e
leh <leandro.programmer@hotmail.com>
2021-10-10 19:31:42 -0300

    criando formulariio createUserModal

--- src/components/Modal/CreateUserModal/index.tsx
+++ src/components/Modal/CreateUserModal/index.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import Modal from 'react-modal'
+import Image from 'next/dist/client/image'
 
-import { X } from '@styled-icons/feather'
+import { Button } from '../../Button'
+
+import { X, Upload, UserPlus } from '@styled-icons/feather'
+import UserDefault from '../../../../public/userDefault.png'
 
 import styles from './styles.module.scss'
 
@@ -9,7 +14,21 @@ interface CreateUserModalProps {
   onRequestClose: () => void
 }
 
-export function CreateUserModal({ isOpen, onRequestClose }: CreateUserModalProps) {
+export function CreateUserModal({
+  isOpen,
+  onRequestClose
+}: CreateUserModalProps) {
+  const [cpf, setCpf] = useState('')
+  const [avatar, setAvatar] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [genderId, setGenderId] = useState(false)
+  const [address, setAddress] = useState('')
+  const [mail, setMail] = useState('')
+  const [mobileNumber, setMobileNumber] = useState('')
+  const [password, setPassword] = useState('')
+  const [state, setState] = useState(false)
+
   return (
     <Modal
       isOpen={isOpen}
@@ -23,6 +42,168 @@ export function CreateUserModal({ isOpen, onRequestClose }: CreateUserModalProps
           <X className={styles.buttonClose} onClick={onRequestClose} />
         </header>
 
+        <form onSubmit={() => {}}>
+          <fieldset>
+            <div className={styles.userCreateLeft}>
+              <div className={styles.inputBlock}>
+                <label htmlFor="cpf">CPF</label>
+                <input
+                  type="text"
+                  id="cpf"
+                  value={cpf}
+                  onChange={event => setCpf(event.target.value)}
+                  placeholder="CPF"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputBlock}>
+                <label htmlFor="avatar">Avatar</label>
+                <input
+                  type="text"
+                  id="avatar"
+                  value={avatar}
+                  onChange={event => setAvatar(event.target.value)}
+                  placeholder="Avatar"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputBlock}>
+                <label htmlFor="firstName">Nome</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  value={firstName}
+                  onChange={event => setFirstName(event.target.value)}
+                  placeholder="Nome"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputBlock}>
+                <label htmlFor="lastName">Sobrenome</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  value={lastName}
+                  onChange={event => setLastName(event.target.value)}
+                  placeholder="Sobrenome"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputBlock}>
+                <label htmlFor="genderId">Gênero</label>
+
+                <div className={styles.selectTypeContainer}>
+                  <button
+                    type="button"
+                    onClick={() => setGenderId(true)}
+                    className={styles.active}
+                  >
+                    Masculino
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => setGenderId(false)}
+                    // className={!masc ? styles.active : styles.disabled}
+                  >
+                    Femenino
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.userCreateRight}>
+              <div className={styles.inputBlock}>
+                <label htmlFor="address">Endereço</label>
+                <input
+                  type="text"
+                  id="address"
+                  value={address}
+                  onChange={event => setAddress(event.target.value)}
+                  placeholder="Endereço"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputBlock}>
+                <label htmlFor="mail">Email</label>
+                <input
+                  type="text"
+                  id="mail"
+                  value={mail}
+                  onChange={event => setMail(event.target.value)}
+                  placeholder="Email"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputBlock}>
+                <label htmlFor="mobileNumber">Número celular</label>
+                <input
+                  type="text"
+                  id="mobileNumber"
+                  value={mobileNumber}
+                  onChange={event => setMobileNumber(event.target.value)}
+                  placeholder="Número celular"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputBlock}>
+                <label htmlFor="password">Contrasenha</label>
+                <input
+                  type="text"
+                  id="password"
+                  value={password}
+                  onChange={event => setPassword(event.target.value)}
+                  placeholder="Contrasenha"
+                  required
+                />
+              </div>
+
+              <div className={styles.inputBlock}>
+                <label htmlFor="state">Estado</label>
+
+                <div className={styles.selectTypeContainer}>
+                  <button
+                    type="button"
+                    onClick={() => setState(true)}
+                    className={styles.active}
+                  >
+                    Ativo
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => setState(false)}
+                    // className={!act ? styles.active : styles.disabled}
+                  >
+                    Inativo
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.formRight}>
+                <div className={styles.formImgContainer}>
+                  <Image src={UserDefault} alt="Avatar" />
+                  <label htmlFor="file">
+                    <Upload />
+                  </label>
+                  <input type="file" id="file" style={{ display: 'none' }} />
+                </div>
+
+                <Button type="submit">
+                  <UserPlus />
+                  Criar
+                </Button>
+              </div>
+          </fieldset>
+        </form>
       </div>
     </Modal>
   )
