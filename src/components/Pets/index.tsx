73e6cc9d708fc32819e61911d41735ce7b580751
joadioz <joaquin.pk.jidp@gmail.com>
2021-10-25 10:22:01 -0300

    Pet con mostrar

--- src/components/Pets/index.tsx
+++ src/components/Pets/index.tsx
@@ -6,93 +6,73 @@ import { Edit2, Heart } from '@styled-icons/feather'
 import { Button } from '../Button'
 
 import styles from './styles.module.scss'
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
 
 interface PetsProps {
-  onOpenUpdatePetModal: () => void
+  onOpenUpdatePetModal: any
   onOpenCreatePetModal: () => void
+}
+interface PetType{
+  petId: string
+  name: string
+  gender: string
+  dateBirth: string
+  size: string
+  breed: string
+  state: boolean
+  createAt: string
+  dueñoMascota:string
 }
 
 export function Pets({ onOpenUpdatePetModal, onOpenCreatePetModal }: PetsProps) {
-  const rows: GridRowsProp = [
-    {
-      id: 1,
-      avatar:
-        'https://e7.pngegg.com/pngimages/623/575/png-clipart-bulldog-computer-icons-pet-others-miscellaneous-pet.png',
-      nombreMascota: 'jordan',
-      generoMascota:'M',
-      nacimientoMascota: '24/10/1943',
-      age: 24,
-      status: 'active'
-    },
-    {
-      id: 2,
-      avatar:
-        'https://e7.pngegg.com/pngimages/623/575/png-clipart-bulldog-computer-icons-pet-others-miscellaneous-pet.png',
-      nombreMascota: 'Negra',
-      generoMascota:'F',
-      nacimientoMascota: '24/10/1943',
-      age: 25,
-      status: 'active'
-    },
-    {
-      id: 3,
-      avatar:
-        'https://e7.pngegg.com/pngimages/623/575/png-clipart-bulldog-computer-icons-pet-others-miscellaneous-pet.png',
-      nombreMascota: 'Kin',
-      generoMascota:'M',
-      nacimientoMascota: '24/10/1943',
-      age: 27,
-      status: 'active'
-    }
-  ]
+  const [pets, setPets] = useState<PetType[]>([])
+
+  useEffect(() => {
+    api.get('/pets').then(response => {
+      setPets(response.data)
+      console.log(response.data)
+    })
+  }, [])
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 100 },
     {
-      field: 'nombreMascota',
-      headerName: 'Nombre',
-      width: 250,
-      renderCell: params => {
-        return (
-          <div className={styles.columnUserName}>
-            <img src={params.row.avatar} alt="Avatar" />
-            {params.row.nombreMascota}
-          </div>
-        )
-      }
+      field: 'name',
+      headerName: 'Nombre de mascota',
+      width: 125
     },
-    { field: 'generoMascota', headerName: 'Genero', width: 130, align:'center' },
     {
-      field: 'nacimientoMascota',
+      field: 'gender',
+      headerName: 'Genero',
+      type: 'text',
+      width: 125
+    },
+    {
+      field: 'dateBirth',
       headerName: 'Nacimiento',
-      type: 'date',
-      width: 180,
-
-    },
-    {
-      field: 'dueñoMascota',
-      headerName: 'Dueño',
       width: 150
     },
     {
-      field: 'status',
-      headerName: 'Status',
+      field: 'breed',
+      headerName: 'Raza',
+      width: 150
+    },
+    {
+      field: 'state',
+      headerName: 'Estado',
       width: 150
     },
     {
       field: 'action',
-      headerName: 'Action',
+      headerName: 'Ação',
       width: 150,
-      renderCell: props => {
+      renderCell: (pet) => {
         return (
           <>
-            {/* <Link passHref href={'/user/' + props.row.id}> */}
-            <Link passHref href="#">
-              <Button onClick={onOpenUpdatePetModal}>
-                <Edit2 className={styles.columnUserButtonEdit} />
-                Editar
-              </Button>
-            </Link>
+            <Button onClick={() => onOpenUpdatePetModal(pet.row.id)} >
+              <Edit2 className={styles.columnUserButtonEdit} />
+              Editar
+            </Button>
           </>
         )
       }
@@ -103,7 +83,7 @@ export function Pets({ onOpenUpdatePetModal, onOpenCreatePetModal }: PetsProps) 
     <div className={styles.container}>
       <div className={styles.content}>
         <DataGrid
-          rows={rows}
+          rows={pets}
           columns={columns}
           pageSize={9}
           checkboxSelection
@@ -112,7 +92,6 @@ export function Pets({ onOpenUpdatePetModal, onOpenCreatePetModal }: PetsProps) 
         />
 
         <Button onClick={onOpenCreatePetModal}>
-          <Heart />
           Criar
         </Button>
       </div>
