797519105f5179041f7203a2790f08e44dcb942e
leh <leandro.programmer@hotmail.com>
2021-11-05 04:28:29 -0300

    upd updateUserModal

--- src/components/Modal/UpdateUserModal/index.tsx
+++ src/components/Modal/UpdateUserModal/index.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState, FormEvent } from 'react'
+import toast from 'react-hot-toast'
 import Modal from 'react-modal'
 import Image from 'next/image'
-import toast from 'react-hot-toast'
-
 import {
   Edit2,
   X,
@@ -15,13 +14,12 @@ import {
   Phone,
   MapPin
 } from '@styled-icons/feather'
-import AvatarImg from '../../../../public/avatar.jpg'
 
 import { Button } from '../../Button'
-
-import styles from './styles.module.scss'
+import AvatarImg from '../../../../public/avatar.jpg'
 
 import api from '../../../services/api'
+import styles from './styles.module.scss'
 
 interface UpdateUserModalProps {
   isOpen: boolean
@@ -76,79 +74,101 @@ export function UpdateUserModal({
   const [state, setState] = useState(false)
 
   useEffect(() => {
-    api.get(`/users/${idUser}`).then(response => {
-      setUser(response.data)
-    })
+    async function selectUserById() {
+      await api.get(`/users/${idUser}`).then(response => {
+        setUser(response.data)
+      })
+    }
 
-    setCpf(user.cpf)
-    setFirstName(user.first_name)
-    setLastName(user.last_name)
-    setMail(user.mail)
-    setMobileNumber(user.mobile_number)
-    setState(user.state)
+    selectUserById()
   }, [idUser])
 
-  function handleOnRequestClose() {
-    setCpf('')
-    setFirstName('')
-    setLastName('')
-    setMail('')
-    setMobileNumber(0)
-    setState(false)
-
-    onRequestClose()
-  }
-
   async function handleSubmit(event: FormEvent) {
-    event.preventDefault()
+    // event.preventDefault()
 
-    console.log(cpf)
-    console.log(firstName)
-    console.log(lastName)
-    console.log(mail)
-    console.log(mobileNumber)
-    console.log(state)
+    let newCpf = ''
+    let newFirstName = ''
+    let newLastName = ''
+    let newMail = ''
+    let newMobileNumber = 0
+    let newState = true
 
-    // await api
-    //   .put(`/users/${idUser}`, {
-    //     cpf: cpf,
-    //     first_name: firstName,
-    //     last_name: lastName,
-    //     mail: mail,
-    //     mobile_number: mobileNumber,
-    //     state: state
-    //   })
-    //   .then(response => {
-    //     console.log(response)
-    //     toast.success('Usuário atualizado com susseso!')
+    if (cpf === '') {
+      newCpf = user.cpf
+    } else {
+      newCpf = cpf
+    }
+    if (firstName === '') {
+      newFirstName = user.first_name
+    } else {
+      newFirstName = firstName
+    }
+    if (lastName === '') {
+      newLastName = user.last_name
+    } else {
+      newLastName = lastName
+    }
+    if (mail === '') {
+      newMail = user.mail
+    } else {
+      newMail = mail
+    }
+    if (mobileNumber === 0) {
+      newMobileNumber = user.mobile_number
+    } else {
+      newMobileNumber = mobileNumber
+    }
+    if (state === user.state) {
+      newState = user.state
+    } else {
+      newState = state
+    }
 
-    //     setCpf('')
-    //     setFirstName('')
-    //     setLastName('')
-    //     setMail('')
-    //     setMobileNumber(0)
-    //     setState(false)
+    try {
+      const response = await api.put(`/users/${idUser}`, {
+        cpf: newCpf,
+        first_name: newFirstName,
+        last_name: newLastName,
+        mail: newMail,
+        mobile_number: newMobileNumber,
+        state: newState
+      })
 
-    //     onRequestClose()
-    //   })
+      const status = response.status
+
+      if (status === 200) {
+        toast.success('Usuário atualizado com susseso!')
+
+        setCpf('')
+        setFirstName('')
+        setLastName('')
+        setMail('')
+        setMobileNumber(0)
+        setState(true)
+
+        onRequestClose()
+      } else {
+        toast.error('Usuário no fue atualizado!')
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={handleOnRequestClose}
+      onRequestClose={onRequestClose}
       className={styles.Modal}
       overlayClassName={styles.Overlay}
     >
       <div className={styles.wrapper}>
-        {/******************************** Header Modal ********************************/}
         <div className={styles.userHeader}>
           <h2>Editar Usuario</h2>
           <X className={styles.buttonClose} onClick={onRequestClose} />
         </div>
 
         <div className={styles.userContainer}>
-          {/******************************** User Info ********************************/}
           <div className={styles.userShow}>
             <header>
               {/* ---------- User Header ---------- */}
@@ -161,7 +181,6 @@ export function UpdateUserModal({
 
             <main>
               <span className={styles.titleMain}>Detalhes do Usuario</span>{' '}
-              {/* - Detalhes do Usuario - */}
               {/* ---------- Nome ---------- */}
               <div className={styles.userInfo}>
                 <User />
@@ -180,7 +199,6 @@ export function UpdateUserModal({
                 <span>{user.year_of_birth}</span>
               </div>
               <span className={styles.titleMain}>Contato</span>{' '}
-              {/* - Contato - */}
               {/* ---------- Endereço ---------- */}
               <div className={styles.userInfo}>
                 <MapPin />
@@ -197,7 +215,6 @@ export function UpdateUserModal({
                 <span>{user.mobile_number}</span>
               </div>
               <span className={styles.titleMain}>Detalhes da Conta</span>{' '}
-              {/* - Detalhes da Conta - */}
               {/* ---------- Estado ---------- */}
               <div className={styles.userInfo}>
                 <UserCheck />
@@ -214,90 +231,100 @@ export function UpdateUserModal({
             <h3>Atualizar Dados</h3>
 
             <form onSubmit={handleSubmit}>
-              {/******************************** Form Left ********************************/}
-              <div className={styles.formLeft}>
-                {/* ---------- CPF ---------- */}
-                <div className={styles.updateItem}>
-                  <label>CPF</label>
-                  <input
-                    type="text"
-                    defaultValue={user.cpf}
-                    onChange={event => setCpf(event.target.value)}
-                  />
-                </div>
-                {/* ---------- Nome ---------- */}
-                <div className={styles.updateItem}>
-                  <label>Nome</label>
-                  <input
-                    type="text"
-                    defaultValue={user.first_name}
-                    onChange={event => setFirstName(event.target.value)}
-                  />
-                </div>
-                {/* ---------- Sobrenome ---------- */}
-                <div className={styles.updateItem}>
-                  <label>Sobrenome</label>
-                  <input
-                    type="text"
-                    defaultValue={user.last_name}
-                    onChange={event => setLastName(event.target.value)}
-                  />
-                </div>
-                {/* ---------- Email ---------- */}
-                <div className={styles.updateItem}>
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    defaultValue={user.mail}
-                    onChange={event => setMail(event.target.value)}
-                  />
-                </div>
-                {/* ---------- Telefone ---------- */}
-                <div className={styles.updateItem}>
-                  <label>Telefone</label>
-                  <input
-                    type="number"
-                    defaultValue={user.mobile_number}
-                    onChange={event =>
-                      setMobileNumber(Number(event.target.value))
-                    }
-                  />
-                </div>
-                {/* ---------- Estado ---------- */}
-                <div className={styles.updateItem}>
-                  <label>Estado</label>
+              <fieldset>
+                <div className={styles.formLeft}>
+                  {/* ---------- CPF ---------- */}
+                  <div className={styles.updateItem}>
+                    <label>CPF</label>
+                    <input
+                      type="text"
+                      value={cpf}
+                      placeholder={user.cpf}
+                      onChange={event => setCpf(event.target.value)}
+                    />
+                  </div>
 
-                  <div className={styles.selectTypeContainer}>
-                    <button
-                      type="button"
-                      onClick={() => setState(true)}
-                      className={user.state ? styles.active : styles.disabled}
-                    >
-                      <span>Ativo</span>
-                    </button>
-                    <button
-                      type="button"
-                      onClick={() => setState(false)}
-                      className={!user.state ? styles.active : styles.disabled}
-                    >
-                      <span>Inativo</span>
-                    </button>
+                  {/* ---------- Nome ---------- */}
+                  <div className={styles.updateItem}>
+                    <label>Nome</label>
+                    <input
+                      type="text"
+                      value={firstName}
+                      placeholder={user.first_name}
+                      onChange={event => setFirstName(event.target.value)}
+                    />
+                  </div>
+
+                  {/* ---------- Sobrenome ---------- */}
+                  <div className={styles.updateItem}>
+                    <label>Sobrenome</label>
+                    <input
+                      type="text"
+                      value={lastName}
+                      placeholder={user.last_name}
+                      onChange={event => setLastName(event.target.value)}
+                    />
+                  </div>
+
+                  {/* ---------- Email ---------- */}
+                  <div className={styles.updateItem}>
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      value={mail}
+                      placeholder={user.mail}
+                      onChange={event => setMail(event.target.value)}
+                    />
+                  </div>
+
+                  {/* ---------- Telefone ---------- */}
+                  <div className={styles.updateItem}>
+                    <label>Telefone</label>
+                    <input
+                      type="number"
+                      value={mobileNumber}
+                      onChange={event =>
+                        setMobileNumber(Number(event.target.value))
+                      }
+                    />
+                  </div>
+
+                  {/* ---------- Estado ---------- */}
+                  <div className={styles.updateItem}>
+                    <label>Estado</label>
+
+                    <div className={styles.selectTypeContainer}>
+                      <button
+                        type="button"
+                        onClick={() => setState(true)}
+                        className={state ? styles.active : styles.disabled}
+                      >
+                        <span>Ativo</span>
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => setState(false)}
+                        className={!state ? styles.active : styles.disabled}
+                      >
+                        <span>Inativo</span>
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              {/******************************** Form Right ********************************/}
-              <div className={styles.formRight}>
-                {/* ---------- Avatar ---------- */}
-                <div className={styles.formImgContainer}>
-                  <Image src={AvatarImg} alt="Avatar" />
+                <div className={styles.formRight}>
+                  {/* ---------- Avatar ---------- */}
+                  <div className={styles.formImgContainer}>
+                    <Image src={AvatarImg} alt="Avatar" />
+                  </div>
                 </div>
-
+              </fieldset>
+              <footer>
                 <Button type="submit">
                   <Edit2 />
                   Atualizar
                 </Button>
-              </div>
+              </footer>
             </form>
           </div>
         </div>
