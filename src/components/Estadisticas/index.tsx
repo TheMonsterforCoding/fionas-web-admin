b365b0628ebdaa4624da7becd8db3980b91ff878
joadioz <joaquin.pk.jidp@gmail.com>
2021-12-19 20:53:27 -0300

    primer paso de servicios y arreglo de side bar por parte del css

---
+++ src/components/Estadisticas/index.tsx
@@ -0,0 +1,11 @@
+
+import styles from './styles.module.scss'
+export function Estadisticas() {
+    return (
+      <div className={styles.container} >
+        <h1 className={styles.h1}>Estadisticas</h1>
+
+      </div>
+    )
+  }
+  
